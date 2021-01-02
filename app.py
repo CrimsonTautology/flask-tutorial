6692068f770d08af17ed2492e8f25636e4a8f8b6
https://github.com/CrimsonTautology <billehs+github@gmail.com>
2021-01-02 13:53:20 -0500

    add config.py and environments

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
+    """GET /"""
     return 'Hello World!'
 
 
 @app.route('/<name>')
 def hello_name(name):
+    """GET /<name>"""
     return f'Hello {name}!'
 
 
